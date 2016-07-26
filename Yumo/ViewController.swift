c1a00ae6b95f251a24939000b7fc8c4de7df3117
AndrewNgKF <andrew.ctrl.alt.del@gmail.com>
2016-07-26 13:50:37 +0800

    Initial Commit

---
+++ Yumo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yumo
+//
+//  Created by IMAC on 26/7/16.
+//  Copyright © 2016 Andrew Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
